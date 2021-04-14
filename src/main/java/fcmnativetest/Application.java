f50d87f749d8b02cc725bcfd3f0f13527b6dc8ac
dzou <dzou@users.noreply.github.com>
2021-04-14 13:46:18 -0400

    graalvm test changes

--- src/main/java/fcmnativetest/Application.java
+++ src/main/java/fcmnativetest/Application.java
@@ -1,17 +1,25 @@
 package fcmnativetest;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import java.io.IOException;
 
 public class Application {
 
     private static final String DEVICETOKEN = System.getenv("FCM_DEVICETOKEN");
 
-    public static void main(String[] args) throws FirebaseMessagingException {
+    public static void main(String[] args) throws FirebaseMessagingException, IOException {
+        FirebaseOptions opts = FirebaseOptions.builder()
+            .setProjectId("my-kubernetes-codelab-217414")
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .build();
+
         System.out.println("Initializing FirebaseApp");
-        FirebaseApp app = FirebaseApp.initializeApp();
+        FirebaseApp app = FirebaseApp.initializeApp(opts);
         System.out.println("FirebaseApp initialized");
 
         Message message = Message.builder()
